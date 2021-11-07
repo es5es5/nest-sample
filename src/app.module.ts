4913b6df8f568b649b84e1d817bf4d4faae09076
es5es5 <es5e5s@nate.com>
2021-11-07 21:37:48 +0900

    typeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
 import { CatsService } from './cats/cats.service';
+import { CatsModule } from './cats/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: 'localhost',
+    //   port: 3306,
+    //   username: 'root',
+    //   password: 'root',
+    //   database: 'test',
+    //   entities: [],
+    //   synchronize: true,
+    // }),
+    // CatsModule,
+  ],
   controllers: [AppController, CatsController],
   providers: [AppService, CatsService],
 })
