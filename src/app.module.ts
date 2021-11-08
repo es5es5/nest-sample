0e770cfe71300bc70f5656d732617fe1d75e2701
es5es5 <es5e5s@nate.com>
2021-11-08 10:20:35 +0900

    typeORM clean

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,20 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { CatsController } from './cats/cats.controller';
-import { CatsService } from './cats/cats.service';
-import { CatsModule } from './cats/users.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    // TypeOrmModule.forRoot({
-    //   type: 'mysql',
-    //   host: 'localhost',
-    //   port: 3306,
-    //   username: 'root',
-    //   password: 'root',
-    //   database: 'test',
-    //   entities: [],
-    //   synchronize: true,
-    // }),
-    // CatsModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'sample_nest',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    UsersModule,
   ],
-  controllers: [AppController, CatsController],
-  providers: [AppService, CatsService],
 })
 export class AppModule {}
--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,7 +1,7 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
-export class Cat {
+export class User {
   @PrimaryGeneratedColumn()
   id: number;
 
